773c3f3aacbe5b35d353ccaa7715766ae1ea0afc
Olya Kostenok <ola.kostenok@gmail.com>
2020-03-11 14:52:27 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let brackets,
+        arr = str;
+
+    brackets = bracketsConfig.map(item => item.reduce((acc, cur) => acc + cur));
+  
+    for (let i = arr.length; i >= 0 ; i--) {
+      brackets.forEach(item => {
+        arr = arr.replace(item, '')
+      });
+      
+      if(arr.length === 0) return true;
+    }
+  
+    return false
 }
